7e4668091b69686c20b643819178657be1370db9
David Guerrero <david.guerrero@quoders.com>
2022-03-24 17:54:34 +0000

    Add tab navigation with Around Me and Profile

---
+++ Ride Mate/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Ride Mate
+//
+//  Created by David Guerrero Diaz on 24/03/2022.
+//
+
+import UIKit
+
+class ProfileViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
